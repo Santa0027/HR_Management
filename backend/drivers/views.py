23e26ccc326b630a8a2eb11f8dc793e3a734129a
santha kumar <vjsanthakumar@gmail.com>
2025-06-11 16:07:51 +0530

    project setup completed

--- backend/drivers/views.py
+++ backend/drivers/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def driver_list(request):
+    data = {
+        "drivers": [
+            {"id": 1, "name": "Santhosh", "phone": "9876543210"},
+            {"id": 2, "name": "Rahul", "phone": "9876509876"},
+        ]
+    }
+    return Response(data)
